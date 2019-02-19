1acad4a6173fec7c2225b8f65ee40f2de82b3518
Valentin Ouvrard <valentin.ouvrard@yahoo.fr>
2019-02-19 17:27:46 +0100

    chore(connection): Add basic connection page

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Image, TextInput, StyleSheet } from 'react-native';
+import ConnectionView from './src/components/connection'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <ConnectionView/>
       </View>
     );
   }
 }
 
+// CSS
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/components/connection/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, Image, TextInput, StyleSheet } from 'react-native';
+import logo from '../../../assets/logo.png'
+
+export default class ConnectionView extends React.Component {
+  state = {
+    username: undefined,
+    password: undefined
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={logo} style={styles.image}/>
+        <View style={styles.form}>
+          <TextInput
+            style={styles.text}
+            placeholder="Nom d'utilisateur / Mail"
+            textAlign={'center'}
+            onChangeText={(username) => this.setState({username})}
+          />
+          <TextInput
+            style={styles.text}
+            placeholder="Mot de passe"
+            textAlign={'center'}
+            onChangeText={(password) => this.setState({password})}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+// CSS
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    resizeMode: 'contain',
+    margin: 5,
+    marginTop: 200,
+    marginBottom: 50
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  form: {
+    flex:3
+  },
+  text: {
+    height: 60,
+    fontSize: 20,
+    textAlign: "center",
+  }
+});
