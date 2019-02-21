4c958a439bb20e9ac195eb6584406d60ecc99648
Valentin Ouvrard <valentin.ouvrard@yahoo.fr>
2019-02-21 11:13:19 +0100

    chore(signup): Add signup action to context

--- src/context/context-provider.js
+++ src/context/context-provider.js
@@ -4,9 +4,11 @@ import { Context } from './context'
 export class ContextProvider extends React.PureComponent {
     state = {
         user: undefined,
-        language: 'Francais',
         achievements: undefined,
         network_status: undefined,
+        error_notifier: undefined,
+        warning_notifier: undefined,
+        info_notifier: undefined,
         history: undefined,
         favorites: undefined,
         routes: undefined
@@ -15,18 +17,45 @@ export class ContextProvider extends React.PureComponent {
     actions = {
         fetchEntity: (name, stateName) => {
             const { dispatch } = this.actions
-            window.fetch('http://sportizen.ml/api/' + name, {
-                method: 'post',
+            fetch('http://sportizen.ml/api/' + name)
+                .then((response) => {
+                    dispatch({network_status : response.status})
+                }).catch((error) => {
+                    console.log('There has been a problem with your fetch operation: ' + error.message)
+                    dispatch({network_status: 'ERROR' + error.message})
+                })
+        },
+
+        signUpUser: (mail, pwd) => {
+            const { dispatch } = this.actions
+            fetch('https://sportizen.ml/signup', {
+                method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    'Accept' : 'application/json'
                 },
-                body: JSON.stringify(this.getData)
+                body: JSON.stringify({
+                    email: mail,
+                    password: pwd,
+                })
             }).then((response) => {
-                dispatch({network_status : response.status})
+                if (response.status === 200) {
+                    console.log('Signed up')
+                    return response.json()
+                } else if (response.status === 400) {
+                    console.log('Signed up error')
+                    return response.json()
+                } else {
+                    console.log('Other error, Status:' + response.status)
+                }
+                dispatch({network_status: response.status})
+            }).then((json) => {
+                console.log(json)
             }).catch((error) => {
                 console.log('There has been a problem with your fetch operation: ' + error.message)
                 dispatch({network_status: 'ERROR' + error.message})
             })
+            
         },
 
         dispatch: (newState) => {
