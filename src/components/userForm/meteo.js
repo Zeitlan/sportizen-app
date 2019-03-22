69c3dde775670c19d3d4a4364976f698c766e91f
mazyad_v <vincent.mazyad@epita.fr>
2019-03-22 16:34:36 +0100

    chore(meteo): finished icons + added general style + correct values from api in layout

--- src/components/userForm/meteo.js
+++ src/components/userForm/meteo.js
@@ -11,7 +11,9 @@ export default class Meteo extends React.Component{
             latitude: 0,
             longitude: 0,
             error: '',
-            forecast: []
+            forecast: [],
+            city: '',
+            api_called: false
         };
         this.getLocation();
     }
@@ -24,7 +26,8 @@ export default class Meteo extends React.Component{
                     (prevState) => ({
                         latitude: position.coords.latitude, 
                         longitude: position.coords.longitude
-                    }), () => { this.getWeather() }
+                    }), () => { this
+                        .getWeather() }
                 )
             },
             (error) => this.setState({ forecast: error.message }),
@@ -35,23 +38,37 @@ export default class Meteo extends React.Component{
     getWeather(){
 
         // Construct the API url to call
-        let url = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + this.state.latitude + '&lon=' + this.state.longitude + '&units=metric&APPID=79d8299eaf52439691aa531853ba88d1';
+        let url = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + this.state.latitude + '&lon=' + this.state.longitude + '&units=metric&lang=fr&APPID=79d8299eaf52439691aa531853ba88d1';
 
         // Call the API, and set the state of the weather forecast
         fetch(url)
             .then(response => response.json())
             .then(data => {
                 this.setState((prevState, props) => ({
-                    forecast: data
+                    forecast: data,
+                    api_called: true
                 }));
             })
     }
 
     render()
     {
-        console.log(this.state)
+        if (this.state.error != '' || !this.state.api_called)
+            return (<Text> loading</Text>)
+
+        else
+            console.log(this.state.date)    
         return(
-            <Text>test</Text>
+            <View>
+                <Text style={{marginTop: 20}}>
+                    {this.state.forecast.list[0].dt_txt}
+                </Text>
+                <View>
+                    <Text>
+                        {this.state.forecast.list[0].main.temp}
+                    </Text>
+                </View>
+            </View>
         )
     }
 }
