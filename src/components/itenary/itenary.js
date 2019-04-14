b42268df798220ac98a0cf7147f1b9f5bcbdb23e
mazyad_v <vincent.mazyad@epita.fr>
2019-04-14 12:40:34 +0200

    chore(itenary): added animation when switching mode

--- src/components/itenary/itenary.js
+++ src/components/itenary/itenary.js
@@ -1,11 +1,30 @@
 /* eslint-disable linebreak-style */
 import React from 'react'
-import { View, Image, TextInput, StyleSheet, KeyboardAvoidingView, Text} from 'react-native'
+import { View, Image, TextInput, StyleSheet, KeyboardAvoidingView, Text, Animated} from 'react-native'
 import destination from '../../../assets/itenary/destination.png'
 import distance from '../../../assets/itenary/distance.png'
 import AutoCompleteInput from './autocomplete'
 
 export default class ItenaryView extends React.Component {
+
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            fadeAnim: new Animated.Value(0)
+        }
+    }
+
+    componentDidMount() {
+        Animated.timing(                  // Animate over time
+            this.state.fadeAnim,            // The animated value to drive
+            {
+                toValue: 1,                   // Animate to opacity: 1 (opaque)
+                duration: 1000,              // Make it take a while
+            }
+        ).start()                        // Starts the animation
+    }
+
     render()
     {   
         return (
@@ -19,7 +38,9 @@ export default class ItenaryView extends React.Component {
                     </View>
                 </View>
                 <View style={{borderBottomColor:'#D3D3D3', borderBottomWidth: 1, marginTop: 10, marginBottom: 15}}></View>
-                <AutoCompleteInput></AutoCompleteInput>
+                <Animated.View style={{opacity: this.state.fadeAnim}}>
+                    <AutoCompleteInput></AutoCompleteInput>
+                </Animated.View>
             </View>
         )
     }
