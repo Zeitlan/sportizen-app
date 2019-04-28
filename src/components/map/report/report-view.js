0f1aa5033b9552994b090fe46637bc2bb0f6ca3b
Valentin Ouvrard <valentin.ouvrard@yahoo.fr>
2019-04-29 00:04:38 +0200

    feat(report): Update report view with images and style

--- src/components/map/report/report-view.js
+++ src/components/map/report/report-view.js
@@ -1,14 +1,37 @@
 // Dependencies
 import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, Image } from 'react-native'
 
 class ReportView extends React.Component {
-    
+    state = {
+        level: 2,
+        commentary: 'Quelques travaux, possible de passer sur le côté mais pas très pratique.',
+        date: '20/03/2018 - 21:30:20',
+        imgs: {
+            level_1: require('../../../../assets/report/report-1-selected.png'),
+            level_2: require('../../../../assets/report/report-2-selected.png'),
+            level_3: require('../../../../assets/report/report-3-selected.png'),
+        },
+    }
+
+    getImgFromLevel = () => {
+        const { level, imgs } = this.state
+        if (level === 1) {
+            return <Image style={styles.reportImg} source={imgs.level_1} />
+        } else if (level === 2) {
+            return <Image style={styles.reportImg} source={imgs.level_2} />
+        } else {
+            return <Image style={styles.reportImg} source={imgs.level_3} />
+        }
+    }
     render() {
         return (
             <View style={styles.informations}>
-                <Text>Travaux</Text>
-                <Text>le 20 Mars 2019 à 21:30:20</Text>
+                {this.getImgFromLevel()}
+                <View style={styles.content}>
+                    <Text style={styles.date}>{this.state.date}</Text>
+                    <Text style={styles.commentary}>{this.state.commentary}</Text>
+                </View>
             </View>)
     }
 }
@@ -18,6 +41,23 @@ export default ReportView
 // CSS
 const styles = StyleSheet.create({
     informations: {
-        width: 200,
+        width: 300,
+        padding: 5,
+        paddingRight: 50,
+        flexDirection:'row', 
+    },
+    content: {
+        paddingLeft: 20,
+    },
+    date: {
+
+    },
+    commentary: {
+        color: '#777777'
+    },
+    reportImg: {
+        height:50,
+        width: 50,
+        resizeMode: 'contain',
     }
 })
