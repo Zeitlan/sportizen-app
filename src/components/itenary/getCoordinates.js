7f9631a2de6d0430442cd9b94b88373476e8d75c
mazyad_v <vincent.mazyad@epita.fr>
2019-05-01 21:52:14 +0200

    feat(autocomplete): coordinates must work

--- src/components/itenary/getCoordinates.js
+++ src/components/itenary/getCoordinates.js
@@ -1,8 +1,8 @@
 /* eslint-disable linebreak-style */
 
-/* return the latitude and longitude position of user input */
+/* return the latitude and longitude position of user input, position argument = 0 if position depart, 1 if not */
 
-export default getCoordinates = (textInput) => {
+export default getCoordinates = (textInput, position) => {
     let url = 'https://api.opencagedata.com/geocode/v1/geojson?q=' + textInput + '&key=ee0f54e8730e40f6ba7736ca09a0d808&language=fr&limit=1'
     return fetch(url)
         .then(response => response.json())
@@ -11,7 +11,7 @@ export default getCoordinates = (textInput) => {
             if (dataJson['status']['code'] != 200) // an error occured
                 return {error: 'Une erreur est survenue'}
             else if (dataJson['features']['length'] === 0) // means no result found
-                return {error: 'Aucun résultat n\'a été trouvé, veuillez ajouter plus de précisions et vérifier que l\'adresse renseignée est valide'}
+                return {error: 'Aucun résultat n\'a été trouvé, veuillez ajouter plus de précisions pour votre adresse ' + ((position == 0)? 'de départ, ' : 'd\'arrivée, ') + 'et vérifier que l\'adresse renseignée est valide'}
             let _latitude =  dataJson['features'][0]['geometry']['coordinates'][1]
             let _longitude = dataJson['features'][0]['geometry']['coordinates'][0]
             return {latitude: _latitude, longitude: _longitude, error: ''}        
--- src/components/itinary/autocomplete.js
+++ src/components/itinary/autocomplete.js
@@ -135,30 +135,32 @@ export default class AutoCompleteInput extends React.Component{
      */
 
     async ValidateData(){
-        if (this.state.textDepart.trim() == '' || this.state.textDepart.trim() == '') // all fields not completed
+        if (this.state.textDepart.trim() == '' || this.state.textArrivee.trim() == '') // all fields not completed
             this.setState({error: 'Tous les champs ne sont pas renseignés, Veuillez remplir tous les champs puis réessayer'})
         else{
             let is_error = false
             let position_depart = undefined
             let position_arrivee = undefined
 
-            await getCoordinates(this.state.textDepart).then((data) => { // get coordinates of text départ
+            await getCoordinates(this.state.textDepart, 0).then((data) => { // get coordinates of text départ
                 let error_ = data.error
                 if (error_ != '') // means an error has occured
                     throw error_
                 position_depart = {latitude: data.latitude, longitude: data.longitude}  // no error  
             }).catch((error_) => {
                 this.setState({error : error_})
-                is_error = true  }) // error occured
+                is_error = true }) // error occured
 
-            await getCoordinates(this.state.textArrivee).then((data) => { // get coordinates of text arrivée
+            await getCoordinates(this.state.textArrivee, 1).then((data) => { // get coordinates of text arrivée
                 let error_ = data.error
                 if (error_ != '') // means an error has occured
                     throw error_
                 position_arrivee = {latitude: data.latitude, longitude: data.longitude}    
-            }).catch((error_) => {this.setState({error : error_})
-                is_error = true  }) // error occured
-            if (is_error == false){
+            }).catch((error_) => {
+                this.setState({error : error_})
+                is_error = true }) // error occured
+
+            if (is_error == false){ // means no errors have occured, we got all coordinates values
                 console.log('position départ ', position_depart) // FIXME ADD NAVIGATION THERE
                 console.log('position arrivée ', position_arrivee)
             }
